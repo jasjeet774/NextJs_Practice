e2ad79822bbda6e5b253ec1c2355bf8fe2687064
jasjeet774 <jasjeetkaur774@gmail.com>
2025-06-02 14:17:57 +0530

    cover topics file structure,routing,nested routing,dynamic routes,nested dynamic routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,8 @@
+export default async function ProductReview({
+    params,
+}:{
+    params:Promise<{productId: string,reviewId:string }>;
+}){
+    const{productId,reviewId} =await params;
+  return <h1>Review {reviewId} of the products {productId}</h1>
+}
