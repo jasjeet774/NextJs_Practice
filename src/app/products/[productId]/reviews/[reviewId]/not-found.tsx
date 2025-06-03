d8acb4cfea016e35fa8ecf66169a21e141990c21
jasjeet774 <jasjeetkaur774@gmail.com>
2025-06-03 14:23:08 +0530

    catch-all-segments and Not found page

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { usePathname } from "next/navigation"
+export default function NotFound(){
+  const pathname=usePathname();
+  const productId=pathname.split("/")[2]
+  const reviewId=pathname.split("/")[4]
+  return <h1>Review {reviewId} for product {productId} not found</h1>
+
+}
