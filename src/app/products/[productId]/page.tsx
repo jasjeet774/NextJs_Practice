62849a22c2a58727e6a9b632274b3738085c31a6
Jasjeet kaur <79755881+jasjeet774@users.noreply.github.com>
2025-07-03 08:04:31 +0000

    Client-Side Navigation with Link Component

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,23 +1,21 @@
-import {Metadata} from "next";
-type Props={
-    params:Promise<{productId: string }>;
-
+import { Metadata } from "next";
+type Props = {
+  params: Promise<{ productId: string }>;
 };
-export const generateMetadata=async({
-    params,
-}:Props) : Promise<Metadata> =>{
-    const id =(await params).productId;
-    const title=await new Promise((resolve)=>{
-        setTimeout(() =>{
-            resolve(`iphone ${id}`);
-        },100);
-        });
-    return {
-        title: `Product ${title}`,
-    };
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${id}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
 };
-export default async function ProductDetails({params}:Props)
-{
-    const productId=(await params).productId;
-    return <h1>Details of the products {productId}</h1>;
-    }
+export default async function ProductDetails({ params }: Props) {
+  const productId = (await params).productId;
+  return <h1>Details of the products {productId}</h1>;
+}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,7 @@
 // app/products/page.tsx
 
+import Link from "next/link";
+
 async function getProducts() {
   const res = await fetch("https://fakestoreapi.com/products");
   return res.json();
@@ -13,7 +15,15 @@ export default async function ProductsPage() {
       <h1>Products</h1>
       <ul>
         {products.map((product: any) => (
-          <li key={product.id}>{product.title}</li>
+          <li key={product.id}>
+            {product.title}
+            <Link
+              href={`/products/${product.id}`}
+              className="text-blue-600 hover:underline"
+            >
+              {product.title}
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
